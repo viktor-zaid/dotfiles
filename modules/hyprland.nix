eca450473e9beb0557da7a2c9dc9c3f290aa707d
levantini4n <hinfluenced@gmail.com>
2025-01-27 19:07:22 +0300

    Added both Hebrew and Arabic to keyboard

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -103,7 +103,9 @@
       };
 
       input = {
-        kb_layout = "us";
+        kb_layout = "us,ara,il";
+        kb_variant = ",";
+        kb_options = "grp:alt_shift_toggle";
         follow_mouse = 1;
         sensitivity = 0;
 
