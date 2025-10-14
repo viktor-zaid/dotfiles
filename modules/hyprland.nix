681045aea808c1b34f3229140da11f25397024fd
levantini4n <hinfluenced@gmail.com>
2025-10-14 10:24:30 +0300

    Not sure what I am committing btw

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -207,7 +207,7 @@
       ];
 
       windowrulev2 = [
-        "suppressevent maximize, class:.*"
+	"suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
     };
